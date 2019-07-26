f4830f15873a5c036246932ac3b983ebc2161060
unknown <wumaoqiang@vip.qq.com>
2019-07-26 16:44:57 +0800

    添加lambda

--- app/src/main/java/com/example/rxjavainvolve/MainActivity.java
+++ app/src/main/java/com/example/rxjavainvolve/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rxjavainvolve;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -18,6 +19,7 @@ import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -30,11 +32,56 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTv;
     private final String BASEURL = "https://wanandroid.com/";
     ApiService mApiService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+//       flatMap();
+        zip();
+
+
+    }
+
+    private void zip() {
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.d(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.d(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.d(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.d(TAG, "emit 4");
+                emitter.onNext(4);
+                Log.d(TAG, "emit complete1");
+                emitter.onComplete();
+            }
+        });
+
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                Log.d(TAG, "emit A");
+                emitter.onNext("A");
+                Log.d(TAG, "emit B");
+                emitter.onNext("B");
+                Log.d(TAG, "emit C");
+                emitter.onNext("C");
+                Log.d(TAG, "emit complete2");
+                emitter.onComplete();
+            }
+        });
+
+        Observable.zip(observable1, observable2, (integer, s) -> integer + s).subscribe(s -> Log.i(TAG, "accept: " + s), throwable -> {
+
+        });
+    }
+
+    private void flatMap() {
+
         Retrofit retrofit = new Retrofit.Builder()
                 //服务器地址
                 .baseUrl(BASEURL)
@@ -44,16 +91,10 @@ public class MainActivity extends AppCompatActivity {
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 //设置OKHttpClient为网络客户端
                 .build();
-           //获取API接口 通过flatMap走了两个请求
+        //获取API接口 通过flatMap走了两个请求
         mApiService = retrofit.create(ApiService.class);
         Observable<RetrofitBean> regist = mApiService.getInfoRxjava();
-        regist.flatMap(new Function<RetrofitBean, ObservableSource<NextBean>>() {
-            @Override
-            public ObservableSource<NextBean> apply(RetrofitBean retrofitBean) throws Exception {
-
-                return mApiService.getInfonextBean();
-            }
-        }).subscribeOn(Schedulers.io())//指定网络请求在io后台线程中进行
+        regist.flatMap((Function<RetrofitBean, ObservableSource<NextBean>>) retrofitBean -> mApiService.getInfonextBean()).subscribeOn(Schedulers.io())//指定网络请求在io后台线程中进行
                 .observeOn(AndroidSchedulers.mainThread())//指定observer回调在UI主线程中进行
                 .subscribe(new Observer<NextBean>() {
                     @Override
@@ -82,5 +123,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mTv = (TextView) findViewById(R.id.tv);
+        mTv.setOnClickListener(v -> {
+
+        });
     }
 }
